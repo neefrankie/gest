88c070a7c545d96a38e3dc531e66d593951b96ed
neefrankie <neefrankie@gmail.com>
2022-03-27 19:30:35 +0800

    test

--- html/html_test.go
+++ html/html_test.go
@@ -5,42 +5,29 @@ import (
 )
 
 func TestNewHTML(t *testing.T) {
+	type args struct {
+		lang string
+	}
 	tests := []struct {
 		name string
-		args Params
-		want *HTML
+		args args
+		want string
 	}{
 		{
 			name: "HTML",
-			args: Params{
-				Lang: "en",
-				Head: nil,
-				Body: nil,
+			args: args{
+				lang: "en",
 			},
-			want: &HTML{
-				Tag{
-					Token:       NewToken("html"),
-					SelfClosing: false,
-					Attr:        NewAttr().Set("lang", "en"),
-					Children: []Node{
-						&Head{},
-						&Body{},
-					},
-				},
-			},
+			want: `<!DOCTYPE html><html lang="en"></html>`,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			got := NewHTML(tt.args.lang)
 
-			got := NewHTML(tt.args)
-
-			if got.String() != tt.want.String() {
+			if got.String() != tt.want {
 				t.Errorf("NewHTML() = %v, want %v", got, tt.want)
-				return
 			}
-
-			t.Logf("%s", got)
 		})
 	}
 }
--- html/tag_test.go
+++ html/tag_test.go
@@ -12,10 +12,10 @@ func TestTag_String(t1 *testing.T) {
 		{
 			name: "Tag string",
 			fields: Tag{
-				Token:       Token{"div"},
-				SelfClosing: false,
-				Attr:        NewAttr().Set("class", "d-flex"),
-				Children:    nil,
+				token:       Token{"div"},
+				selfClosing: false,
+				attr:        NewAttr().Set("class", "d-flex"),
+				children:    nil,
 			},
 			want: `<div class="d-flex"></div>`,
 		},
@@ -23,10 +23,10 @@ func TestTag_String(t1 *testing.T) {
 	for _, tt := range tests {
 		t1.Run(tt.name, func(t1 *testing.T) {
 			t := &Tag{
-				Token:       tt.fields.Token,
-				SelfClosing: tt.fields.SelfClosing,
-				Attr:        tt.fields.Attr,
-				Children:    tt.fields.Children,
+				token:       tt.fields.token,
+				selfClosing: tt.fields.selfClosing,
+				attr:        tt.fields.attr,
+				children:    tt.fields.children,
 			}
 			if got := t.String(); got != tt.want {
 				t1.Errorf("String() = %v, want %v", got, tt.want)
