9b0dc2d22b947054b9167bb5e0255ea9ebc4b7c7
neefrankie <neefrankie@gmail.com>
2022-03-27 19:28:27 +0800

    refactor: token

--- html/token.go
+++ html/token.go
@@ -1,18 +1,9 @@
 package html
 
-type Token string
+type Token struct {
+	Literal string
+}
 
-const (
-	TokenHTML Token = "html"
-	TokenHead       = "head"
-	TokenBody       = "body"
-	TokenP          = "p"
-	TokenDiv        = "div"
-	TokenH1         = "h1"
-	TokenH2         = "h2"
-	TokenH3         = "h3"
-	TokenH4         = "h4"
-	TokenH5         = "h5"
-	TokenH6         = "h6"
-	TokenImg        = "img"
-)
+func NewToken(name string) Token {
+	return Token{name}
+}
